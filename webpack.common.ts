1820bcf4a08e8bb8775ed24002b149e6cc5c346b
TotallyNot <44345987+TotallyNot@users.noreply.github.com>
2020-12-30 01:18:43 +0100

    initial commit

---
+++ webpack.common.ts
@@ -0,0 +1,32 @@
+import { join } from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+    entry: {
+        background: join(__dirname, "src/background/index.ts"),
+    },
+    output: {
+        path: join(__dirname, "dist/js"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.tsx?$/,
+                use: "ts-loader",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+        alias: {
+            common: join(__dirname, "src/common"),
+        },
+    },
+    node: {
+        fs: "empty",
+    },
+};
+
+export default config;
